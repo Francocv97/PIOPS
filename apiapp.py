63364c8b6988fd97279bb16c099413c6829d46c3
Franco Calderón <128433301+Francocv97@users.noreply.github.com>
2023-12-27 19:11:38 -0300

    Add files via upload

--- apiapp.py
+++ apiapp.py
@@ -9,6 +9,8 @@ from collections import Counter
 import gzip
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
 
 
 # Ruta a tu archivo JSON comprimido
@@ -170,7 +172,78 @@ def read_sentiment_analysis(year: int):
     return sentiment_dict
 
 
+# Elimina las filas con valores NaN en la columna de juegos
+df1 = df1.dropna(subset=['title'])
 
+# Crea una matriz de características utilizando TfidfVectorizer
+tfidf = TfidfVectorizer(stop_words='english')
+tfidf_matrix = tfidf.fit_transform(df1['title'])
+
+# Calcula la similitud del coseno
+cosine_sim_titulo = linear_kernel(tfidf_matrix, tfidf_matrix)
+
+# Crea una serie para mapear los índices de los juegos a sus títulos
+indices_titulo = pd.Series(df1.index, index=df1['title']).drop_duplicates()
+
+def recomendacion_juego(titulo, cosine_sim=cosine_sim_titulo):
+    # Obtiene el índice del juego que coincide con el título
+    idx = indices_titulo[titulo]
+
+    # Obtiene las puntuaciones de similitud por pares de todos los juegos con ese juego
+    sim_scores = list(enumerate(cosine_sim[idx]))
+
+    # Ordena los juegos en función de las puntuaciones de similitud
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+
+    # Obtiene las puntuaciones de los 5 juegos más similares
+    sim_scores = sim_scores[1:6]
+
+    # Obtiene los índices de los juegos
+    juego_indices = [i[0] for i in sim_scores]
+
+    # Devuelve los 5 juegos más similares
+    return df1['title'].iloc[juego_indices]
+
+@app.get("/recomendacion_titulo/{titulo}")
+def get_recomendacion_titulo(titulo: str):
+    # Llama a la función de recomendación de juego
+    juegos_recomendados = recomendacion_juego(titulo)
+
+    # Devuelve los juegos recomendados
+    return {"Juegos recomendados para el título {}: {}".format(titulo, list(juegos_recomendados))}
+
+# Carga los datos para el segundo modelo de recomendación
+df_usuario = pd.read_csv('2dafuncion_final.csv')
+
+# Crea la matriz de utilidad usando 'playtime_forever' como valor
+utilidad = df_usuario.pivot_table(index='user_id', columns='title', values='playtime_forever')
+
+# Calcula la similitud del coseno
+similitud_usuario = cosine_similarity(utilidad.fillna(0))
+
+# Crea un mapeo de ID de usuario a índice de matriz
+user_id_to_index = {user_id: index for index, user_id in enumerate(utilidad.index)}
+
+def recomendacion_usuario(user_id):
+    # Obtiene el índice de la matriz para el ID de usuario
+    user_index = user_id_to_index[user_id]
+    # Obtiene los índices de los 6 usuarios más similares
+    indices_similares = np.argsort(similitud_usuario[user_index])[-7:-1][::-1]
+    # Encuentra los juegos que les gustaron a los usuarios similares
+    juegos_recomendados = utilidad.iloc[indices_similares].mean().sort_values(ascending=False).index[:6]
+    # Excluye el primer juego (el más recomendado) y devuelve los siguientes 5
+    return juegos_recomendados[1:]
+
+@app.get("/recomendacion_usuario/{user_id}")
+def get_recomendacion_usuario(user_id: str):
+    if user_id in user_id_to_index:
+        # Llama a la función de recomendación de usuario
+        juegos_recomendados = recomendacion_usuario(user_id)
+
+        # Devuelve los juegos recomendados
+        return {"Juegos recomendados para el usuario {}: {}".format(user_id, list(juegos_recomendados))}
+    else:
+        return {"error": "El ID de usuario {} no se encuentra en los datos.".format(user_id)}
 
 # http://localhost:8000/UserForGenre/Action
 # http://localhost:8000/PlayTimeGenre/Casual
