66bdbef4fa1baa9c84058ea1ae7bb14a86a793fc
Franco Calderón <128433301+Francocv97@users.noreply.github.com>
2023-12-27 23:02:03 -0300

    Add files via upload

--- apiapp.py
+++ apiapp.py
@@ -1,29 +1,29 @@
 import json
 import pandas as pd
 import gzip
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from fastapi import FastAPI
 
 app = FastAPI()
 
-# Ruta a tu archivo JSON comprimido
 archivo = 'output_steam_games_limpio_reducido.json.gz'
 
+def read_json_gzip(file_path):
+    with gzip.open(file_path, 'r') as file:
+        data = file.read().decode('utf-8')
+        if data[0] == '[':
+            for item in json.loads(data):
+                yield item
+        else:
+            for line in data.splitlines():
+                yield json.loads(line)
+
 # Lista para guardar cada fila
-lista = []
+lista = list(read_json_gzip(archivo))
 
-with gzip.open(archivo, 'r') as file:
-    data = file.read().decode('utf-8')
-    if data[0] == '[':
-        # Los datos están en formato de array
-        lista = json.loads(data)
-    else:
-        # Los datos están separados por nuevas líneas
-        for line in data.splitlines():
-            lista.append(json.loads(line))
-
-# Crear un DataFrame a partir de la lista
+# Crea un DataFrame a partir de la lista
 df1 = pd.DataFrame(lista)
 
 # Elimina las filas con valores NaN en la columna de juegos
@@ -33,20 +33,19 @@ df1 = df1.dropna(subset=['title'])
 count = CountVectorizer()
 count_matrix = count.fit_transform(df1['title'])
 
-# Calcula la similitud del coseno
-cosine_sim_titulo = cosine_similarity(count_matrix, count_matrix)
-
 # Crea una serie para mapear los índices de los juegos a sus títulos
 indices_titulo = pd.Series(df1.index, index=df1['title']).drop_duplicates()
+
+# Libera memoria no utilizada
+del lista
+
+cosine_sim_titulo = cosine_similarity(count_matrix, count_matrix)
 
 @app.get("/recomendacion_titulo/{titulo}")
 def get_recomendacion_titulo(titulo: str):
     idx = indices_titulo[titulo]
-    sim_scores = list(enumerate(cosine_sim_titulo[idx]))
-    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
-    sim_scores = sim_scores[1:6]
-    juego_indices = [i[0] for i in sim_scores]
-    juegos_recomendados = df1['title'].iloc[juego_indices]
+    sim_scores = np.argsort(cosine_sim_titulo[idx])[::-1][1:6]
+    juegos_recomendados = df1['title'].iloc[sim_scores]
     
     return {"Juegos recomendados para el título {}: {}".format(titulo, list(juegos_recomendados))}
 
